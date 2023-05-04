409217bd4ab117083f8403972f394cd76310297a
Aleksandar <acakovin@live.com>
2023-05-03 21:38:55 -0400

    main task seated up

---
+++ src/Day1_selfPracticing/SimpleTask.java
@@ -0,0 +1,80 @@
+package Day1_selfPracticing;
+/*
+public class SimpleTask {
+
+    private String firstName, lastName;
+    private final int age;
+    private final String homeTown;
+    private String hairColor;
+    private int salary;
+
+    public SimpleTask(String firstName, String lastName, int age, String homeTown, String hairColor, int salary) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        this.age = age;
+        if (age <= 14){
+            System.err.println("Age must be over 14");
+            System.exit(1);
+        }
+        this.homeTown = homeTown;
+        setHairColor(hairColor);
+        setSalary(salary);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getHomeTown() {
+        return homeTown;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", homeTown='" + homeTown + '\'' +
+                ", hairColor='" + hairColor + '\'' +
+                ", salary= $" + salary +
+                '}';
+    }
+
+
+
+
+
+}*/
